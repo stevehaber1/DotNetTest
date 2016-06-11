ae985ec1401f20685d0c661aed36abaa7928b448
stevehaber1 <steve.t.haber@gmail.com>
2016-06-10 22:46:40 -0400

    Lovely new line

--- hwapp/Program.cs
+++ hwapp/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("ANother line");
+            
         }
     }
 }
